c077561855898e0a96d597b98145aca0db7a9e8e
yutuo <yutuo5@gmail.com>
2018-04-11 13:29:20 +0900

    添加 禁用表情图标 功能

--- wp_switch_util.php
+++ wp_switch_util.php
@@ -33,6 +33,7 @@ class WPSwitchUtilConfig
         'autopcontent' => '0',
         'autopcomment' => '0',
 		'reguseronly' => '0',
+        'disableemoji' => '0',
     );
 	
 	/** 注册用户页面 */
@@ -223,7 +224,25 @@ class WPSwitchUtil
         global $error;
         $error = __('Only registered and logged in users are allowed to view this site. Please log in now.', 'wp_su');
 	}
-	
+
+    /** 删除表情 */
+    function disableEmojis() {
+        remove_action('wp_head', 'print_emoji_detection_script', 7);
+        remove_action('admin_print_scripts', 'print_emoji_detection_script');
+        remove_action('wp_print_styles', 'print_emoji_styles');
+        remove_action('admin_print_styles', 'print_emoji_styles');
+        remove_filter('the_content_feed', 'wp_staticize_emoji');
+        remove_filter('comment_text_rss', 'wp_staticize_emoji');
+        remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+        add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
+    }
+    function disable_emojis_tinymce( $plugins ) {
+        if (is_array( $plugins )) {
+            return array_diff($plugins, array('wpemoji'));
+        } else {
+            return array();
+        }
+    }
     /** 应用插件 */
     function apply()
     {
@@ -285,6 +304,10 @@ class WPSwitchUtil
         if ($this->getOption('reguseronly') == '1') {
 			add_action('wp', array($this, 'regUserOnly'));
             add_action('init', array($this, 'loginFormMessage'));
+        }
+        // 删除表情
+        if ($this->getOption('disableemoji') == '1') {
+            add_action('init', array($this, 'disableEmojis'));
         }
     }
 
